8356c6c2aa6eb11daed63e887eb0a176547dea8e
Szilárd Pfeiffer <hellowindow@pfeifferszilard.hu>
2010-03-19 07:51:45 +0100

    Added some section to Dialog related part.

---
+++ hello_window/sources/dialog_create.hpp
@@ -0,0 +1,7 @@
+Gtk::Dialog::Dialog ( )
+
+Gtk::Dialog::Dialog ( const Glib::ustring & title,
+                      Gtk::Window& parent,
+                      bool modal = false,
+                      bool use_separator = false
+                    )
