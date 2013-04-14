f67eede7f1a1f91bce54020fe64b48f99306b20c
Szilárd Pfeiffer <hellowindow@pfeifferszilard.hu>
2013-04-14 13:02:10 +0200

    Improved development environment related article.

--- sources/window_minimal.c
+++ sources/window_minimal.c
@@ -1,16 +1,16 @@
-#include <gtk/gtk.h>
+#include <gtk/gtk.h> /*@ \label{windowminimalc:include} @*/
 
 int
-main(int argc, char *argv[])
+main(int argc, char *argv[]) /*@ \label{windowminimalc:main} @*/
 {
   GtkWidget *window; /*@ \label{windowminimalc:windowdeclare} @*/
 
-  gtk_init (&argc, &argv);
+  gtk_init (&argc, &argv); /*@ \label{windowminimalc:gtkmain} @*/
 
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL); /*@ \label{windowminimalc:windownew} @*/
-  gtk_widget_show (window);
+  gtk_widget_show (window); /*@ \label{windowminimalc:windowshow} @*/
 
   gtk_main (); /*@ \label{windowminimalc:run} @*/
 
-  return 0;
+  return 0; /*@ \label{windowminimalc:return} @*/
 }
--- sources/window_minimal.cc
+++ sources/window_minimal.cc
@@ -1,16 +1,16 @@
-#include <gtkmm.h>
+#include <gtkmm.h> /*@ \label{windowminimalcc:include} @*/
 
 int
-main(int argc, char *argv[])
+main(int argc, char *argv[]) /*@ \label{windowminimalcc:main} @*/
 {
 
 
-  Gtk::Main kit(argc, argv);
+  Gtk::Main kit(argc, argv); /*@ \label{windowminimalcc:gtkmain} @*/
 
   Gtk::Window window; /*@ \label{windowminimalcc:windowdeclare} @*/ /*@ \label{windowminimalcc:windownew} @*/
-  window.show();
+  window.show(); /*@ \label{windowminimalcc:windowshow} @*/
 
   Gtk::Main::run(); /*@ \label{windowminimalcc:run} @*/
 
-  return 0;
+  return 0; /*@ \label{windowminimalcc:return} @*/
 }
